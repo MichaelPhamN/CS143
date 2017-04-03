bbf4a2cc9fecd4cbb588faebe5498b2c675397ce
ppham02219 <michaelphamn@gmail.com>
2017-04-03 10:19:12 -0700

    as

--- Experiement040317/src/HelloExceptions.java
+++ Experiement040317/src/HelloExceptions.java
@@ -1,9 +1,12 @@
 
-public class HelloExceptions {
+public class HelloExceptions{
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		int a = 3;
+		System.out.println("Hello World");
+		throw new NullPointerException("Our little exception");
+//		System.out.println("After the exception");
 	}
 
 }
