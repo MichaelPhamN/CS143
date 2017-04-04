7dede2839a4743be55c070c1e83254e1220dc8cf
ppham02219 <michaelphamn@gmail.com>
2017-04-04 10:21:43 -0700

    04/04/17

--- Experiement040317/src/HelloExceptions.java
+++ Experiement040317/src/HelloExceptions.java
@@ -1,12 +1,20 @@
-
 public class HelloExceptions{
-
+	public static void printLength(String s){		
+		System.out.println(s.length());
+	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int a = 3;
-		System.out.println("Hello World");
-		throw new NullPointerException("Our little exception");
-//		System.out.println("After the exception");
+		try{
+			System.out.println("Hello World");
+			printLength("123456");
+			String s = null;
+			printLength(s);
+			throw new RuntimeException("Our little exception");
+		}catch(Exception e){
+			System.out.println("Oops! Exception happened.");
+			e.printStackTrace(System.out);
+		}
+		System.out.println("After the try/catch block");
 	}
 
 }
