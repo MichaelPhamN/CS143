57b69ba2baec64fabf99a84ab06ca40b0e61d3ae
ppham02219 <michaelphamn@gmail.com>
2017-04-05 10:18:02 -0700

    a

---
+++ Experiement040317/src/Complexity.java
@@ -0,0 +1,27 @@
+
+public class Complexity {
+
+	public static int sum(int[] a) {
+		int s = 0;
+		for (int i = 0; i < a.length; i++) {
+			s = s + a[i];
+		}
+		return s;
+	}
+
+	public static void main(String[] args) {
+		for (int i = 1000000; i < 2000000000; i *= 10) {
+			long totalTime = 0;
+			int trials = 10;
+			for (int j = 0; j < trials; j++) {
+				int[] a = new int[i];
+				long start = System.currentTimeMillis();
+				sum(a);
+				long finish = System.currentTimeMillis();
+				totalTime = totalTime + finish-start;
+			}
+			System.out.println(i+": "+totalTime/trials);
+		}
+	}
+
+}
--- Experiement040317/src/HelloExceptions.java
+++ Experiement040317/src/HelloExceptions.java
@@ -1,20 +1,78 @@
-public class HelloExceptions{
-	public static void printLength(String s){		
-		System.out.println(s.length());
+
+public class HelloExceptions {
+
+	/**
+	 * Prints the length of the string. Throws an exception if s is null.
+	 * @param s non-null string
+	 */
+	public static void printLength(String s) throws Exception {
+		try {
+			System.out.println(s.length());
+			System.out.println("Do we see this?");
+			return;
+		} finally {
+			System.out.println("This is guaranteed to run");
+		}
 	}
+	
+	
+	public static int divide(int a, int b) {
+		if (b == 0) {
+			throw new ArithmeticException("Division by 0 not allowed");
+		}
+		return a / b;
+	}
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		try{
-			System.out.println("Hello World");
-			printLength("123456");
-			String s = null;
-			printLength(s);
-			throw new RuntimeException("Our little exception");
-		}catch(Exception e){
-			System.out.println("Oops! Exception happened.");
+		//int i = 0;
+		try {
+			printLength("12345");
+			printLength(null);
+			System.out.println("Hello");
+		} catch (Throwable e) {
+			System.out.println("Oops!");
 			e.printStackTrace(System.out);
 		}
-		System.out.println("After the try/catch block");
+		System.out.println("Goodbye");
 	}
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//public class HelloExceptions{
+//	public static void printLength(String s){		
+//		System.out.println(s.length());
+//	}
+//	public static void main(String[] args) {
+//		// TODO Auto-generated method stub
+//		try{
+//			System.out.println("Hello World");
+//			printLength("123456");
+//			String s = null;
+//			printLength(s);
+//			throw new RuntimeException("Our little exception");
+//		}catch(Exception e){
+//			System.out.println("Oops! Exception happened.");
+////			e.printStackTrace(System.out);
+//		}
+//		System.out.println("After the try/catch block");
+//	}
+//
+//}
