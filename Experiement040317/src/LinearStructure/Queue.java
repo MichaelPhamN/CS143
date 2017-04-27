621105ba87b33b5282b6cc6d6b321cc7d9d8c5d5
michaelphamn@gmail.com <Phuc1986>
2017-04-26 23:44:23 -0700

    a

--- Experiement040317/src/LinearStructure/Queue.java
+++ Experiement040317/src/LinearStructure/Queue.java
@@ -1,8 +1,10 @@
 package LinearStructure;
 public class Queue {
 	private InternalQueue queue;
+	private InternalQueue end; // Who is the last person in line?
 	public Queue() {
 		queue = null;
+		end = null;
 	}
 	
 	public String remove() {
@@ -12,6 +14,9 @@ public class Queue {
 		}
 		String first = queue.getData();
 		queue = queue.getRest();
+		if (queue == null) {
+			end = null; // Nobody is last in line
+		}
 		return first;
 	}
 	
@@ -19,11 +24,12 @@ public class Queue {
 		InternalQueue q = queue;
 		if (q == null) {
 			queue = new InternalQueue(s, null);
+			end = queue; // The last person is the only person in line
 		} else {
-			while (q.getRest() != null) {
-				q = q.getRest();
-			}
+			q = end;
 			q.setRest(new InternalQueue(s, null));
+			// Change the last thing in line
+			end = end.getRest(); // Actual last person is the person after the old last person
 		}		
 		
 	}
--- Experiement040317/src/LinearStructure/QueueTest.java
+++ Experiement040317/src/LinearStructure/QueueTest.java
@@ -3,13 +3,28 @@ public class QueueTest {
 
 	public static void main(String[] args) {
 		Queue queue = new Queue();
-		queue.add("a");
+		for (int i = 0; i < 100000000; i++) {
+			queue.add(String.valueOf(i));
+		}
+//		String s;
+//		int i = 0;
+//		// Prints a, b, c in order added
+//		while ((s = queue.remove()) != null) {
+//			if (Integer.parseInt(s) != i) {
+//				System.out.println("Unexpected!");
+//			}
+//			i++;
+//		}
+		/*
 		queue.add("b");
 		queue.add("c");
 		String s;
+		// Prints a, b, c in order added
 		while ((s = queue.remove()) != null) {
 			System.out.println(s);
 		}
+		*/
+		System.out.println("Done");
 	}
 
 }
